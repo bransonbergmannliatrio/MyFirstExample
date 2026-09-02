182f9d920821f2cf762e30d20cc07804d9ea75a9
Branson Bergmann <branson.bergmann@liatrio.com>
2026-09-02 11:48:01 -0700

    merge

--- main.cpp
+++ main.cpp
@@ -8,17 +8,14 @@ int main()
 
     int x,y;
 
-    cin >> x >> y;
-    cout << "Addition: " << x + y << endl;
-    cout << "Subtraction: " << x - y << endl;
-    cout << "Multiplication: " << x * y << endl;
-if (y == 0) {
-        cout << "Division: undefined (cannot divide by zero)" << endl;
-    } else {
-        cout << "Division: " << x / y << endl;
-    }    cout << "Remainder: " << x % y << endl;
-    cout << "Square Root: " << sqrt(x) << endl;
-    cout << "Square: " << pow(x, y) << endl;
+    std::cin >> x >> y;
+    std::cout << "Addition: " << x + y << std::endl;
+    std::cout << "Subtraction: " << x - y << std::endl;
+    std::cout << "Multiplication: " << x * y << std::endl;
+    std::cout << "Division: " << x / y << std::endl;
+    std::cout << "Remainder: " << x % y << std::endl;
+    std::cout << "Square Root: " << sqrt(x) << std::endl;
+    std::cout << "Square: " << pow(x, y) << std::endl;
 
     return 0;
 }
